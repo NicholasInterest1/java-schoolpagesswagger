82e12198ee5211e04586a29ae06ac3e5b23c610d
Nicholas Interest <NicholasInterest1@users.noreply.github.com>
2019-10-15 19:02:52 -0400

    MVP

--- school/src/main/java/com/lambdaschool/school/service/CourseServiceImpl.java
+++ school/src/main/java/com/lambdaschool/school/service/CourseServiceImpl.java
@@ -1,46 +1,42 @@
 package com.lambdaschool.school.service;
 
+import com.lambdaschool.school.exceptions.ResourceNotFoundException;
 import com.lambdaschool.school.model.Course;
 import com.lambdaschool.school.repository.CourseRepository;
 import com.lambdaschool.school.view.CountStudentsInCourses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 
 @Service(value = "courseService")
-public class CourseServiceImpl implements CourseService
-{
+public class CourseServiceImpl implements CourseService {
+
     @Autowired
     private CourseRepository courserepos;
 
     @Override
-    public ArrayList<Course> findAll()
-    {
+    public ArrayList<Course> findAll(Pageable pageable) {
         ArrayList<Course> list = new ArrayList<>();
-        courserepos.findAll().iterator().forEachRemaining(list::add);
+        courserepos.findAll(pageable).iterator().forEachRemaining(list::add);
         return list;
     }
 
     @Override
-    public ArrayList<CountStudentsInCourses> getCountStudentsInCourse()
-    {
+    public ArrayList<CountStudentsInCourses> getCountStudentsInCourse() {
         return courserepos.getCountStudentsInCourse();
     }
 
     @Transactional
     @Override
-    public void delete(long id) throws EntityNotFoundException
-    {
-        if (courserepos.findById(id).isPresent())
-        {
+    public void delete(long id) throws ResourceNotFoundException {
+        if (courserepos.findById(id).isPresent()) {
             courserepos.deleteCourseFromStudcourses(id);
             courserepos.deleteById(id);
-        } else
-        {
-            throw new EntityNotFoundException(Long.toString(id));
+        } else {
+            throw new ResourceNotFoundException(Long.toString(id));
         }
     }
 }
